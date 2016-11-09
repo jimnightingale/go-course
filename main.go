53f9129aa9532c5be130e8a46f7dcdcf0deee28e
jimnightingale <nightingalejames@yahoo.co.uk>
2016-11-09 21:25:33 +0000

    functions demo

--- main.go
+++ main.go
@@ -10,30 +10,39 @@ func main() {
 
 	gridLoad := 75.
 
-	fmt.Println("1) Generate Power Plant Report")
-	fmt.Println("2) Generate Power Grid Report")
-	fmt.Print("Please choose an option: ")
+	menuOption := menu()
 
-	var option string
-
-	fmt.Scanln(&option)
-
-	switch option {
+	switch menuOption {
 	case "1":
-		for idx, cap := range plantCapacities {
-			fmt.Printf("Plant %d capacity: %.0f\n", idx, cap)
-		}
+		generatePlantCapacityReport(plantCapacities...)
 	case "2":
-		capacity := 0.
-		for _, plantId := range activePlants {
-			capacity += plantCapacities[plantId]
-		}
-
-		fmt.Printf("%-20s%.0f\n", "capacity: ", capacity)
-		fmt.Printf("%-20s%.0f\n", "Load: ", gridLoad)
-		fmt.Printf("%-20s%.1f%%\n", "Utilization: ", gridLoad/capacity*100)
-
+		generateGridLoadReport(activePlants, plantCapacities, gridLoad)
 	default:
 		fmt.Println("Unknown option, no option taken")
 	}
+}
+
+func generatePlantCapacityReport(plantCapacities ...float64) {
+	for idx, cap := range plantCapacities {
+		fmt.Printf("Plant %d capacity: %.0f\n", idx, cap)
+	}
+}
+
+func generateGridLoadReport(activePlants []int, plantCapacities []float64, gridLoad float64) {
+	capacity := 0.
+	for _, plantId := range activePlants {
+		capacity += plantCapacities[plantId]
+	}
+
+	fmt.Printf("%-20s%.0f\n", "capacity: ", capacity)
+	fmt.Printf("%-20s%.0f\n", "Load: ", gridLoad)
+	fmt.Printf("%-20s%.1f%%\n", "Utilization: ", gridLoad/capacity*100)
+}
+
+func menu() (option string) {
+	fmt.Println("1) Generate Power Plant Report")
+	fmt.Println("2) Generate Power Grid Report")
+	fmt.Print("Please choose an option: ")
+	fmt.Scanln(&option)
+	return
 }
